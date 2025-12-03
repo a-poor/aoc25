12f207d39f492f1c28952f311a9aa86957021563
Austin Poor <45295232+a-poor@users.noreply.github.com>
2025-12-03 09:56:02 -0800

    Day 3 part 2

--- main.go
+++ main.go
@@ -7,33 +7,53 @@ import (
 	"strconv"
 )
 
+const ndigits = 12
+
 func main() {
 	var total int
 
 	// Scan the input
 	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
+		// Get the next line
 		ln := s.Text()
 		cs := []rune(ln)
 
-		// Find the best first character
-		var i1 int
-		for i := 1; i < len(ln)-1; i++ {
-			if cs[i] > cs[i1] {
-				i1 = i
-			}
-		}
+		// An array to store the digits
+		ds := make([]rune, 0, ndigits)
 
-		// Find the best second character
-		i2 := i1 + 1
-		for i := i2 + 1; i < len(ln); i++ {
-			if cs[i] > cs[i2] {
-				i2 = i
+		// Start the loop
+		lasti := -1
+		for len(ds) < ndigits {
+			// Find the best character...
+
+			// Start looking at the next character
+			// with the base case being that the first
+			// remaining character is best
+			bi := lasti + 1
+
+			// Define the loop bounds (to compare against
+			// the base case) starting with the character
+			// *after* next and ending with enough characters
+			// so that we will still have enough to finish
+			// our 12-digit (or n-digit) number
+			start := bi + 1
+			end := len(ln) - (ndigits - len(ds) - 1)
+
+			// Look for better numbers?
+			for i := start; i < end; i++ {
+				if cs[i] > cs[bi] {
+					bi = i
+				}
 			}
+
+			// Push the digit
+			ds = append(ds, cs[bi])
+			lasti = bi
 		}
 
 		// Get the number
-		n, err := strconv.Atoi(string(cs[i1]) + string(cs[i2]))
+		n, err := strconv.Atoi(string(ds))
 		if err != nil {
 			panic(err)
 		}
