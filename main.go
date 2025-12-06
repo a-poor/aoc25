fca43e89f6ced10ca14f1a7b5150e3a7fa471e5b
Austin Poor <45295232+a-poor@users.noreply.github.com>
2025-12-06 10:46:24 -0800

    Day 6 part 1

--- main.go
+++ main.go
@@ -5,186 +5,125 @@ import (
 	"fmt"
 	"os"
 	"regexp"
-	"slices"
 	"strconv"
 	"strings"
+	"unicode"
+)
+
+type operation rune
+
+const (
+	Mul operation = '*'
+	Add operation = '+'
+	Div operation = '/'
+	Sub operation = '-'
 )
 
 func main() {
-	// Read in the input ranges and
-	// ids to check
-	rs, _ := readInput()
-	// fmt.Printf("Read in %d ranges\n", len(rs))
-	compacted := true
-	for compacted {
-		// fmt.Println("Compacting...")
-		rs, compacted = compact(rs)
+	var total int
+	eqs := readInput()
+	for _, eq := range eqs {
+		total += eq.solve()
 	}
-
-	// fmt.Printf("%+v\n", rs)
-
-	// Count up the id size
-	var count int
-	for _, r := range rs {
-		count += r.size()
-	}
-
-	// Done!
-	fmt.Printf("There were %d unique ranges with a total id count of %d\n", len(rs), count)
+	fmt.Printf("total = %d\n", total)
 }
 
-func compact(rs []idrange) ([]idrange, bool) {
-	var compacted bool
-	var rs2 []idrange
-	for _, ra := range rs {
-		// Hit will track if we see one
-		// in the new list that matches
-		hit := -1
+type equation struct {
+	ns []int
+	op operation
+}
 
-		// Look for overlap
-		for j, rb := range rs2 {
-			// if ra.isbefore(rb) {
-			// 	continue
-			// }
-			// if ra.isafter(rb) {
-			// 	break
-			// }
-			if ra.overlaps(rb) {
-				hit = j
-				break
-			}
-		}
-
-		// Did it match one?
-		switch hit {
-		case -1:
-			rs2 = append(rs2, ra)
+func (e equation) solve() int {
+	n := e.ns[0]
+	for i := 1; i < len(e.ns); i++ {
+		switch e.op {
+		case Mul:
+			n *= e.ns[i]
+		case Add:
+			n += e.ns[i]
+		case Div:
+			n /= e.ns[i]
+		case Sub:
+			n -= e.ns[i]
 		default:
-			rs2[hit] = rs2[hit].merge(ra)
-			compacted = true
+			panic(fmt.Sprintf("Unknown rune %q", string(e.op)))
 		}
 	}
-	return rs2, compacted
+	return n
 }
 
-type idrange struct{ start, end int }
-
-func (r idrange) isbefore(o idrange) bool {
-	return r.end < o.start
-}
-
-func (r idrange) isafter(o idrange) bool {
-	return r.start > o.end
-}
-
-func (r idrange) contains(n int) bool {
-	return n >= r.start && n <= r.end
-}
-
-func (r idrange) size() int {
-	return r.end - r.start + 1
-}
-
-func (r idrange) overlaps(o idrange) bool {
-	return r.contains(o.start) ||
-		r.contains(o.end) ||
-		o.contains(r.start) ||
-		o.contains(r.end)
-
-}
-
-func (r idrange) merge(o idrange) idrange {
-	if !r.overlaps(o) {
-		panic(fmt.Sprintf("%+v and %+v don't overlap", r, o))
-	}
-	start := r.start
-	if o.start < start {
-		start = o.start
-	}
-	end := r.end
-	if o.end > end {
-		end = o.end
-	}
-	return idrange{
-		start: start,
-		end:   end,
-	}
-}
-
-func readInput() ([]idrange, []int) {
-	// Pre-define regex for range
-	rre := regexp.MustCompile(`^(\d+)-(\d+)$`)
-
-	// Pre-define arrays to be returned and a
-	// flag to check which list we're on
-	var rs []idrange
-	var ids []int
-	var inids bool
-
-	// Scan through the input lines
+func readInput() []equation {
+	var eqs []equation
 	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
-		// Get the next line
-		ln := s.Text()
+		// Read the next line
+		line := s.Text()
 
+		// Handle different line cases
 		switch {
-		case strings.TrimSpace(ln) == "":
-			// Empty line means switch list type
-			inids = true
-
-		case !inids:
-			// Line is a range
-			gs := rre.FindStringSubmatch(ln)
-			if gs == nil {
-				panic("error finding regex match with '" + ln + "'")
-			}
-			sl, sr := gs[1], gs[2]
-
-			// Parse as numbers
-			nl, err := strconv.Atoi(sl)
-			if err != nil {
-				panic("failed to parse number '" + sl + "': " + err.Error())
-			}
-			nr, err := strconv.Atoi(sr)
-			if err != nil {
-				panic("failed to parse number '" + sr + "': " + err.Error())
+		case len(eqs) == 0:
+			ns := parseNumberLine(line)
+			for _, n := range ns {
+				eqs = append(eqs, equation{
+					ns: []int{n},
+					op: operation(0),
+				})
 			}
 
-			// Add it to the list
-			rs = append(rs, idrange{start: nl, end: nr})
-
-		case inids:
-			// Line is an id candidate
-			//
-			// Just parse it!
-			n, err := strconv.Atoi(ln)
-			if err != nil {
-				panic("Failed to parse id '" + ln + "':" + err.Error())
+		case isNumberLine(line):
+			ns := parseNumberLine(line)
+			for i, n := range ns {
+				eqs[i].ns = append(eqs[i].ns, n)
 			}
-			ids = append(ids, n)
+
+		default:
+			os := parseOpLine(line)
+			for i, o := range os {
+				eqs[i].op = o
+			}
 		}
 	}
 	if err := s.Err(); err != nil {
 		panic(err)
 	}
+	return eqs
+}
 
-	// Sort the ranges
-	slices.SortFunc(rs, func(ra, rb idrange) int {
-		if ra.start < rb.start {
-			return -1
+func isNumberLine(ln string) bool {
+	for _, r := range []rune(ln) {
+		if unicode.IsDigit(r) {
+			return true
 		}
-		if ra.start > rb.start {
-			return 1
-		}
-		if ra.end < rb.end {
-			return -1
-		}
-		if ra.end > rb.end {
-			return 1
-		}
-		return 0
-	})
+	}
+	return false
+}
 
-	// Return the input data!
-	return rs, ids
+func parseNumberLine(ln string) []int {
+	ln = strings.TrimSpace(ln)
+	var ns []int
+	re := regexp.MustCompile(`^\d+`)
+	for len(ln) > 0 {
+		m := re.FindString(ln)
+		n, err := strconv.Atoi(m)
+		if err != nil {
+			panic(err)
+		}
+		ns = append(ns, n)
+		ln = ln[len(m):]
+		ln = strings.TrimSpace(ln)
+	}
+	return ns
+}
+
+func parseOpLine(ln string) []operation {
+	ln = strings.TrimSpace(ln)
+	var os []operation
+	re := regexp.MustCompile(`^[+\-*/]`)
+	for len(ln) > 0 {
+		m := re.FindString(ln)
+		os = append(os, operation(m[0]))
+		ln = ln[len(m):]
+		ln = strings.TrimSpace(ln)
+	}
+	return os
 }
